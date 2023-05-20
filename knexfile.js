2fd5727bdd51d56c2ce4f089350e4b8d500ef10d
MechanicTC2 <sciencenatekwok@gmail.com>
2023-05-20 12:23:51 -0700

    init

---
+++ knexfile.js
@@ -0,0 +1,61 @@
+// Update with your config settings.
+const mysql = require('mysql');
+
+
+var localSQLConnection = {
+  host: 'aws-hackathon-db.cjdmtmc8kuwg.us-west-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'rUDJfSkFAU9N9WXRHMDd',
+  database: 'root',
+};
+
+// Production database connection
+const SQLConnection = localSQLConnection;
+
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection: SQLConnection,
+    migrations: {
+      directory: './database/migrations',
+      tablename: 'knlex_migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+    useNullAsDefault: true,
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  localSQLConnection
+};
---
+++ models/userModel.js
@@ -0,0 +1,77 @@
+var {localSQLConnection} = require('../knexfile');
+const knex = require('knex')({
+    client: 'mysql',
+    connection: localSQLConnection,
+  })
+const bcrypt = require('bcrypt');
+//login
+async function login(req, res, next, err, results) {
+    var email = req.body.email; 
+    var password = req.body.password;
+    console.log("email: " + email)
+    console.log("password: " + password);
+    try {
+        if (email == undefined || password == undefined) {
+            console.log("not enough cred");
+            return "not enough cred";
+        }
+        else {
+            var data = await knex('users').where({
+                username: email
+            }).select('*');
+            console.log("email" + data);
+            if (email != data[0].username || password != data[0].user_password) {
+               console.log("login failed, incorrect pass/email");
+               return "incorrect pass/email";
+            }
+            else {
+                console.log("login success");
+                await bcrypt.genSalt(1, async function(err, salt) {
+                    await bcrypt.hash(password, salt, async function(err, hash) {
+                        res.cookie('password', hash);
+                        res.render('welcome', {title: `Welcome to Healther, ${email}`, info: 'What will you do today?'});
+                    });
+                });
+             }
+        }
+    }
+    catch (err) {
+        console.log(err);
+        console.log("login failed, user does not exist");
+        return "user does not exist";
+    }
+}
+//signup
+async function signup(req, res, next) {
+    var email = req.body.email;
+    var password = req.body.password;
+    if (email == undefined || password == undefined) {
+        console.log("signup failed, not enough cred");
+        return "not enough cred";
+    }
+    else {
+        try {
+            var data = await knex('users').where({
+            username: email
+            }).select('*');
+            if (email == data[0].email) {
+                console.log("signup failed, email already exists");
+                return "email already exists";
+            }
+        }
+        catch (error) {
+            knex('users').insert({
+                username: email,
+                user_password: password,
+            });                
+            console.log("signup success");
+            await bcrypt.genSalt(1, async function(err, salt) {
+                await bcrypt.hash(password, salt, async function(err, hash) {
+                    res.cookie('password', hash);
+                    return "signup success";
+                });
+            });
+        }
+    }
+}
+module.exports = {login, signup};
