4504149190d317642b3d317a2b2d51f0ebc26dfb
BuildTools <unconfigured@null.spigotmc.org>
2023-05-20 19:00:33 -0700

    guys i made the gpt functions part 2 remastered v2.0

---
+++ models/aiModel.js
@@ -0,0 +1,32 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: "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"
+});
+const openai = new OpenAIApi(configuration);
+
+async function getNutritionInfo(gender, age, height, weight){
+	
+	const response = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: "format both DRI and micronutrients into a javascript object: " + gender + ", " + age + " year old, " + height + ", " + weight + " pounds.",
+		max_tokens: 3000,
+		temperature: 0,
+	});
+	//console.log(response.data.choices[0].text)
+	return response.data.choices[0].text
+}
+
+async function getDiet(gender, age, height, weight){
+	
+	const response = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: "create a food diet based on this information: " + gender + ", " + age + " year old, " + height + ", " + weight + " pounds." + " format this into a javascript object",
+		max_tokens: 3000,
+		temperature: 0,
+	});
+	//console.log(response.data.choices[0].text)
+	return response.data.choices[0].text
+}
+
+module.exports = {getNutritionInfo, getDiet}
