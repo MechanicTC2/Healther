e888957e005c7b46611cd69194101411fdb6a15e
Timothy Liu <timmytime5671@outlook.com>
2023-05-21 04:34:05 -0700

    healthper to healther

--- routes/index.js
+++ routes/index.js
@@ -9,26 +9,26 @@ router.use(cookieParser());
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  res.render('index', {title: 'Login to Healthper'});
+  res.render('index', {title: 'Login to Healther'});
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', {title: 'Login to Healthper', info: 'Login with existing credentials:'});
+  res.render('login', {title: 'Login to Healther', info: 'Login with existing credentials:'});
 });
 router.get('/signup', function(req, res, next) {
-  res.render('signup', {title: 'Signup for Healthper', info: 'Signup for Healthper, email and password:'});
+  res.render('signup', {title: 'Signup for Healther', info: 'Signup for Healther, email and password:'});
 });
 router.post('/authenticateUser', async function(req, res, next) {
   const result = await login(req, res, next);
   console.log(result);
   if (result == "not enough cred")
-    res.render('login', {title: 'Login to Healthper', info: 'Please fill in all fields. '});
+    res.render('login', {title: 'Login to Healther', info: 'Please fill in all fields. '});
   else if (result == "incorrect pass/email")
-    res.render('login', {title: 'Login to Healthper', info: 'Incorrect password or email:'});
+    res.render('login', {title: 'Login to Healther', info: 'Incorrect password or email:'});
   else if (result == "user does not exist")
-    res.render('login', {title: 'Login to Healthper', info: 'Email does not exist. If you do not have an account, please sign up:'});
+    res.render('login', {title: 'Login to Healther', info: 'Email does not exist. If you do not have an account, please sign up:'});
   else if (result == "ETIMEDOUT")
-    res.render('login', {title: 'Login to Healthper', info: 'Please check your connection and try again. If the issue persists, try reconnecting to the internet:'});
+    res.render('login', {title: 'Login to Healther', info: 'Please check your connection and try again. If the issue persists, try reconnecting to the internet:'});
   else {
     bcrypt.genSalt(1, async function(err, salt) {
       bcrypt.hash(result.substring(result.indexOf(";") + 1), salt, async function(err, hash) {
